0c761e0f7d085ac6938f3d35fbdfc464fdb743e8
wiisportsresort <devdoge1@gmail.com>
2020-08-21 00:27:39 -0400

    add jokes from official_joke_api

--- src/commands/joke.ts
+++ src/commands/joke.ts
@@ -4,30 +4,51 @@ import { Message } from 'discord.js';
 import * as he from 'he';
 import { Command } from '.';
 import { CmdArgs } from '../types';
+import { hasFlags } from '../util';
 
 export class CommandJoke implements Command {
   cmd = 'joke';
-  docs = {
-    usage: 'joke',
-    description: 'Get a joke from Codepen',
-  };
-  getJoke(): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-      axios('https://codepen.io/pen/')
-        .then(res => {
-          if (!res) return reject();
-          if (res.status !== 200) return reject();
+  docs = [
+    {
+      usage: 'joke',
+      description: 'get a progamming joke from https://github.com/15Dkatz/official_joke_api',
+    },
+    {
+      usage: 'joke -g',
+      description: 'get a generic joke from https://github.com/15Dkatz/official_joke_api',
+    },
+    {
+      usage: 'joke -c',
+      description: 'Get a joke from codepen (may take a while)',
+    },
+  ];
+  async getCodepenJoke() {
+    const response = await axios.get('https://codepen.io/pen/');
+    if (response.status !== 200) throw new Error("response code " + response.status);
 
-          const text = cheerio.load(res.data)('#loading-text').html();
-          if (!text) return reject('no text in #loading-text');
-          resolve(he.decode(text.replace('\n', '').replace(/<\/?code>/g, '`')));
-        })
-        .catch(err => reject(err));
-    });
+    const text = cheerio.load(response.data)('#loading-text').html();
+    if (!text) throw new Error('no text in #loading-text');
+
+    return (he.decode(text.replace('\n', '').replace(/<\/?code>/g, '`')));
   }
-  async executor({ msg }: CmdArgs): Promise<void | Message> {
+  async getGenericJoke() {
+    const response = await axios.get('https://official-joke-api.appspot.com/jokes/random');
+    return `${response.data.setup}\n${response.data.punchline}`;
+  }
+  async getProgrammingJoke() {
+    const response = await axios.get(
+      'https://official-joke-api.appspot.com/jokes/programming/random'
+    );
+    return `${response.data[0].setup}\n${response.data[0].punchline}`;
+  }
+  async executor({ msg, flags }: CmdArgs): Promise<void | Message> {
     try {
-      const joke = await this.getJoke();
+      let joke: string;
+
+      hasFlags(flags, ['-c', '--codepen']) && (joke ??= await this.getCodepenJoke());
+      hasFlags(flags, ['-g', '--generic']) && (joke ??= await this.getGenericJoke());
+      joke ??= await this.getProgrammingJoke();
+
       return msg.channel.send(joke);
     } catch (err) {
       return msg.channel.send(`Error getting joke, error message: \n\`\`\`\n${err}\n\`\`\``);
