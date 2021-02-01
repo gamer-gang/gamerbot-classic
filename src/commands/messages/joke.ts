510203ba5483d61a7f4cef2e7aa608b9ca62b9d0
wiisportsresort <devdoge1@gmail.com>
2021-02-01 16:59:37 -0500

    goodbye codepen joke

--- src/commands/messages/joke.ts
+++ src/commands/messages/joke.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
 import { Message } from 'discord.js';
-import he from 'he';
 import yargsParser from 'yargs-parser';
 import { Command } from '..';
 import { Context } from '../../types';
@@ -27,19 +25,9 @@ export class CommandJoke implements Command {
     },
     {
       usage: 'joke -c',
-      description: 'get a joke from codepen (may take a while)',
+      description: 'jk no more codepen because they started handing out fat captchas',
     },
   ];
-
-  async getCodepenJoke(): Promise<string> {
-    const response = await axios.get('https://codepen.io/pen/');
-    if (response.status !== 200) throw new Error('response code ' + response.status);
-
-    const text = cheerio.load(response.data)('#loading-text').html();
-    if (!text) throw new Error('no text in #loading-text');
-
-    return he.decode(text.replace(/\n/g, '').replace(/<\/?code>/g, '`'));
-  }
 
   private makeUrl = (type: string) =>
     `https://jokeapi.dev/joke/${type}?format=txt&blacklistFlags=religious,political`;
@@ -59,7 +47,9 @@ export class CommandJoke implements Command {
     try {
       let joke: string;
 
-      args.codepen && (joke ??= await this.getCodepenJoke());
+      if (args.codepen)
+        return msg.channel.send(Embed.error('codepen jokes were removed because captchas :('));
+
       args.programming && (joke ??= await this.getProgrammingJoke());
       joke ??= await this.getGenericJoke();
 
