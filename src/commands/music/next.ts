06e0e0cae447c2437d65c9a1e616e6275965ad0f
wiisportsresort <devdoge1@gmail.com>
2021-02-14 00:20:41 -0500

    next/prev more than 1 track

--- src/commands/music/next.ts
+++ src/commands/music/next.ts
@@ -6,12 +6,18 @@ import { codeBlock, Embed } from '../../util';
 
 export class CommandNext implements Command {
   cmd = ['next', 'skip'];
-  docs: CommandDocs = {
-    usage: ['next'],
-    description: 'skip current track',
-  };
+  docs: CommandDocs = [
+    {
+      usage: 'next',
+      description: 'skip current track',
+    },
+    {
+      usage: 'next <number>',
+      description: 'skip many tracks',
+    },
+  ];
   async execute(context: Context): Promise<void | Message> {
-    const { msg } = context;
+    const { msg, args } = context;
     const queue = client.queues.get(msg.guild.id);
 
     if (!queue.playing) return msg.channel.send(Embed.error('Not playing'));
@@ -21,10 +27,23 @@ export class CommandNext implements Command {
       return msg.channel.send(Embed.error('You are not in the music channel'));
 
     try {
+      if (args._[0] != undefined) {
+        const int = parseInt(args._[0]);
+
+        if (!int || isNaN(int) || int <= 0 || int >= queue.tracks.length - queue.index)
+          return msg.channel.send(Embed.error('Invalid skip amount'));
+
+        queue.index += int - 1;
+
+        msg.channel.send(Embed.success(`Skipped **${int}** tracks`));
+      } else {
+        msg.react('⏮️');
+      }
+
       // break out of looping if looping one
       if (queue.loop === 'one') queue.index++;
+
       queue.voiceConnection?.dispatcher?.end();
-      msg.react('⏭️');
     } catch (err) {
       return msg.channel.send(Embed.error(codeBlock(err)));
     }
--- src/commands/music/previous.ts
+++ src/commands/music/previous.ts
@@ -5,13 +5,19 @@ import { Context } from '../../types';
 import { codeBlock, Embed } from '../../util';
 
 export class CommandPrevious implements Command {
-  cmd = ['previous', 'prev'];
-  docs: CommandDocs = {
-    usage: ['previous'],
-    description: 'go back a track',
-  };
+  cmd = ['previous', 'prev', 'rewind'];
+  docs: CommandDocs = [
+    {
+      usage: ['previous'],
+      description: 'rewind 1 track',
+    },
+    {
+      usage: 'previous <number>',
+      description: 'rewind many tracks',
+    },
+  ];
   async execute(context: Context): Promise<void | Message> {
-    const { msg } = context;
+    const { msg, args } = context;
     const queue = client.queues.get(msg.guild.id);
 
     if (!queue.playing) return msg.channel.send(Embed.error('Not playing'));
@@ -21,11 +27,24 @@ export class CommandPrevious implements Command {
       return msg.channel.send(Embed.error('You are not in the music channel'));
 
     try {
+      if (args._[0] != undefined) {
+        const int = parseInt(args._[0]);
+
+        if (!int || isNaN(int) || int <= 0 || int > queue.index)
+          return msg.channel.send(Embed.error('Invalid amount'));
+
+        queue.index -= int + 1;
+
+        msg.channel.send(Embed.success(`Rewinded **${int}** tracks`));
+      } else {
+        queue.index -= 2;
+        msg.react('⏮️');
+      }
+
       // break out of looping if looping one
       if (queue.loop === 'one') queue.index--;
+
       queue.voiceConnection?.dispatcher?.end();
-      queue.index -= 2;
-      msg.react('⏮️');
     } catch (err) {
       return msg.channel.send(Embed.error(codeBlock(err)));
     }
