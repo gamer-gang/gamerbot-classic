9022d68750ed84794704b43d7a565e70770ad511
wiisportsresort <devdoge1@gmail.com>
2021-06-08 20:20:31 -0400

    add purge starting from a given message

--- src/commands/moderation/purge.ts
+++ src/commands/moderation/purge.ts
@@ -1,41 +1,69 @@
 import { Message, PermissionString, TextChannel } from 'discord.js';
+import yargsParser from 'yargs-parser';
 import { Command, CommandDocs } from '..';
 import { Context } from '../../types';
-import { Embed } from '../../util';
+import { Embed, getDateFromSnowflake } from '../../util';
 
 export class CommandPurge implements Command {
   cmd = 'purge';
-  docs: CommandDocs = {
-    usage: 'purge <number>',
-    description:
-      'delete last `number` messages (might take a while becuase ratelimiting)\n' +
-      'minimum 2, maximum 1000 (command is repeatable)\n' +
-      'only deletes messages up to 14 days old because of a discord limitation',
+  docs: CommandDocs = [
+    {
+      usage: 'purge <number>',
+      description:
+        'delete last `number` messages (might take a while becuase ratelimiting)\n' +
+        'minimum 2, maximum 1000 (command is repeatable)\n' +
+        'only deletes messages up to 14 days old because of a discord limitation',
+    },
+    {
+      usage: 'purge -t, --to <id>',
+      description: 'purge the given message and everything after it',
+    },
+  ];
+  yargs: yargsParser.Options = {
+    alias: { to: 't' },
+    string: ['to'],
   };
   userPermissions: PermissionString[] = ['MANAGE_MESSAGES'];
   botPermissions: PermissionString[] = ['MANAGE_MESSAGES'];
   async execute(context: Context): Promise<void | Message> {
     const { msg, args } = context;
 
-    const n = parseInt(args._[0], 10);
+    let numberToDelete = parseInt(args._[0], 10);
 
-    if (!n || isNaN(n) || n < 2 || n > 1000)
+    // TODO add support for replying to start message when v13 drops
+    if (args.to) {
+      const messages = await msg.channel.messages.fetch();
+      const startMessage = messages.get(args.to.toString());
+      if (!startMessage)
+        return msg.channel.send(
+          Embed.error('Could not resolve starting message in current channel')
+        );
+
+      if (getDateFromSnowflake(args.to.toString()).diffNow().as('days') > 14)
+        return msg.channel.send(Embed.error('Start of range is older than 14 days'));
+
+      const messageArray = messages
+        .array()
+        .sort((a, b) => (a.id as unknown as number) - (b.id as unknown as number));
+      const startIndex = messageArray.indexOf(startMessage);
+      if (startIndex === -1) throw new Error('Start index is -1');
+
+      numberToDelete = messageArray.length - startIndex;
+    }
+
+    if (!numberToDelete || isNaN(numberToDelete) || numberToDelete < 2 || numberToDelete > 1000)
       return msg.channel.send(Embed.error('Number must be an integer from 2 to 1000 inclusive'));
 
-    msg.channel.startTyping();
-
-    for (let i = 0; i < Math.ceil(n / 100); i++) {
-      const deletable = i === Math.floor(n / 100) ? n % 100 : 100;
+    for (let i = 0; i < Math.ceil(numberToDelete / 100); i++) {
+      const deletable = i === Math.floor(numberToDelete / 100) ? numberToDelete % 100 : 100;
       const deleted = await (msg.channel as TextChannel).bulkDelete(deletable, true);
-      if (deleted.array().length < deletable) {
+      if (deleted.size < deletable) {
         msg.channel
           .send(Embed.warning('Stopped deleting because messages are older than 14 days'))
           .then(m => setTimeout(() => m.delete(), 5000));
         return;
       }
-      await new Promise(resolve => setTimeout(resolve, 4000));
+      await new Promise(resolve => setTimeout(resolve, 2500));
     }
-
-    msg.channel.stopTyping();
   }
 }
